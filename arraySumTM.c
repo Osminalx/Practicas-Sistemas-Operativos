05b0fc8f209ef14572f14badf33ac06113d436d4
Osminalx <verdestro64@gmail.com>
2024-02-26 19:49:13 -0800

    Ejercicio 3 terminado

--- arraySumTM.c
+++ arraySumTM.c
@@ -4,9 +4,10 @@
 #include <sys/wait.h>
 
 #define NumOfThreads 4
-#define MaxArrLen 10
+#define MaxArrLen 8
 
 int Array[MaxArrLen];
+int finalSum;
 
 pthread_mutex_t the_mutex;
 
@@ -26,7 +27,22 @@ void printArr(int *array){
     }
 }
 
-void arrSum(){
+void *arrSum(void *thread){
+    int locSum = 0;
+    int tId = thread;
+    int sep = MaxArrLen/NumOfThreads;
+    int init = tId * sep;
+    int end =(tId +1 )*sep;
+    printf("hola soy el thread %d",tId);
+    for (int i = init; i < end; i++)
+    {
+        locSum+=Array[i];
+    }
+    printf("sumalocal: %d \n",locSum);
+    pthread_mutex_lock(&the_mutex);
+    finalSum += locSum;
+    pthread_mutex_unlock(&the_mutex);
+    pthread_exit(NULL);
 }
 
 
@@ -34,5 +50,19 @@ int main(int argc, char const *argv[])
 {
     randGen(Array);
     printArr(Array);
+    pthread_t T[NumOfThreads];
+    pthread_mutex_init(&the_mutex,0);
+    finalSum=0;
+    for (int i = 0; i < NumOfThreads; i++)
+    {
+        pthread_create(&T[i],NULL,arrSum,(void *)i);
+    }
+
+    for (int j = 0; j < NumOfThreads; j++)
+    {
+        pthread_join(T[j],NULL);
+    }
+    printf("Suma final: %d \n",finalSum);
+    pthread_mutex_destroy(&the_mutex);
     return 0;
 }
