5499416997988be8948fa437c11675e1a4e07f85
Osminalx <verdestro64@gmail.com>
2024-03-13 19:27:56 -0700

    Assembly  comparator

--- Ensamblador/Practica2/msgs.asm
+++ Ensamblador/Practica2/msgs.asm
@@ -1,33 +1,41 @@
-        section .data
-msg1: db 'El numero es positivo',10
-ms1: equ $-msg1
-msg2:db 'El numero es negativo',10
-ms2: equ $-msg2
-
-    section .text
+section .data
+    ms1: DB 'El numero es positivo',10
+    m1l: EQU $-ms1
+    ms2: DB 'El numero es negativo',10
+    m2l: EQU $-ms2
+    ms3: DB 'El numero es igual a cero',10
+    m3l: EQU $-ms3
+;jg je jl jz jnz jge jle
+section .text
     global _start
--_start:
-    mov eax,5
+_start:
+    mov eax,0
     mov ebx,0
     cmp eax,ebx
-    jg bigger
-    mov eax,4
-    mov ebx, 1
-    mov ecx,msg2
-    mov edx,ms2
-    int 0x80
-
-bigger:
+    jz Cero
+    jg positivo
     mov eax,4
     mov ebx,1
-    mov ecx,msg1
-    mov edx,ms1
+    mov ecx,ms2
+    mov edx,m2l
+    int 80h
+    jmp continuar
+
+positivo:
+    mov eax,4
+    mov ebx,1
+    mov ecx,ms1
+    mov edx,m1l
     int 0x80
 
-end:
+Cero:
+    mov eax,4
+    mov ebx,1
+    mov ecx,ms3
+    mov edx, m3l
+    int 0x80
+
+continuar:
     mov eax,1
     mov ebx,0
     int 0x80
-
-    section .bss
-    
