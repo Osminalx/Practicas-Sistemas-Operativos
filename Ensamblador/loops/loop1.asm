709d4786f4b94e7c1342f35cc8bc28e2059bd69c
Osminalx <verdestro64@gmail.com>
2024-03-20 19:43:29 -0700

    Loops final

--- Ensamblador/loops/loop1.asm
+++ Ensamblador/loops/loop1.asm
@@ -1,6 +1,7 @@
 section .data
 msg:  db 'Entrandoal loop',10
 ms1: equ $-msg
+ptr: db 
 
     section .text
     global _start    
@@ -9,14 +10,25 @@ _start:
     mov ecx,9
     
 ciclo:
-    dec ecx
-    mov ecx,temp
+    mov [con],ecx
+    mov eax,48
+    add eax, [con]
+    mov [con],eax
+
+    ;;imprimir
     mov eax,4
     mov ebx,1
-    mov ecx,ms1
-    mov edx,m1l
+    mov ecx,con
+    ;;mov ecx,ptr
+    mov eax,1
     int 0x80
-    mov temp,ecx
+    ;;fin impresion
+    mov eax,[con]
+    sub eax,48
+    mov [con],eax
+    
+    mov ecx,[con]
+    dec ecx
     jnz ciclo
 fin:
     mov eax,1
@@ -24,4 +36,3 @@ fin:
     int 0x80
     section .bss
     con resb 1
-    tmp temp 0
