179fc421f230673cbf71630948cbbacfff4ada81
Osminalx <verdestro64@gmail.com>
2024-04-08 18:52:28 -0700

    NO jala el fibo

--- Ensamblador/Fibonacci/fibo.asm
+++ Ensamblador/Fibonacci/fibo.asm
@@ -6,7 +6,8 @@ section .text
 _start:
     mov eax,[bas0]
     mov ebx,[bas1]
-    mov ecx,6
+    ;Numero para calcular
+    mov ecx,8
 fib:
     mov edx,eax
     add eax,ebx
@@ -14,18 +15,28 @@ fib:
     dec ecx
     cmp ecx,1
     jg fib
-    add eax,48
-    mov [bas1],eax
     ;imprimir
-    mov eax,4
+    mov ebx,100
+    mov ecx,0
+converloop:
+    mov edx,0
+    div ebx
+    add dl,'0'
+    mov [esp + ecx],dl
+    inc ecx
+    cmp eax,0
+    jnz converloop
+
+;imprimir digitos
+    mov edx,ecx
+    mov ecx,esp
     mov ebx,1
-    mov ecx,bas1
-    mov edx,1
-    int 0x80
+    mov eax,4
     
     mov eax,1
     mov ebx,0
     int 0x80
+    
 
 
 
