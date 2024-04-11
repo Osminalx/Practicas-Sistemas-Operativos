c217e8f3262ab328659582fefbb9570d9928fde9
Osminalx <verdestro64@gmail.com>
2024-04-10 19:39:15 -0700

    YA funciona el print en fibonacci

--- Ensamblador/Fibonacci/fibo.asm
+++ Ensamblador/Fibonacci/fibo.asm
@@ -1,6 +1,9 @@
 section .data
     bas0: dd 0
     bas1: dd 1
+    divider: dd 0
+    con: dd 0
+    final:dd 0
 section .text
     global _start
 _start:
@@ -17,21 +20,41 @@ fib:
     jg fib
     mov [bas1],eax
     ;imprimir
-imploop:
-    ;imprimir centenas
+flags:
+    mov ecx,100
+    mov [divider],ecx
+    mov ecx,0
+    mov ecx,3
+print:
+    ;contador
+    mov[con],ecx
+    mov edx,0
+    mov eax,0
+    ;division
     mov eax,[bas1]
-    div 100
-    mul 100
-    mov ebx,[bas1]
-    sub ebx,eax
-    mov [bas1],ebx
-    mov edx,eax
-    ;Impresión primer digito
+    mov ebx,[divider]
+    div ebx
+    ;preparar impresion
+    mov [bas1],edx
+    add eax,'0'
+    mov [final],eax
+    ;impresion
     mov eax,4
     mov ebx,1
-    mov ecx,edx
-    ;Imprimir decenas
-    mov eax,
-    div 10
-    sub eax,edx
-    
+    mov ecx,final
+    mov edx,1
+    int 0x80
+    ;actualizar division
+    mov edx,0
+    mov ebx,10
+    mov eax,[divider]
+    div ebx
+    mov [divider],eax
+    ;actualizar contador
+    mov ecx,[con]
+    dec ecx
+    jnz print
+
+    mov eax,1
+    mov ebx,0
+    int 0x80
--- Ensamblador/Producto punto/prp.asm
+++ Ensamblador/Producto punto/prp.asm
@@ -39,15 +39,6 @@ loop:
     add eax,'0'
     mov [sum],eax
 
-    mov eax,4
-    mov ebx,1
-    mov ecx,sum
-    mov edx,1
-    int 0x80
-
-    mov eax,1
-    mov ebx,0
-    int 0x80
 
 section .bss
     sum resd 1
